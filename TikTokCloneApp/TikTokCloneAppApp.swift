643c5720f45c7c00dc8961fea1fa3be2d92296d7
Nguyen Thanh Vinh <vinhnt@aloapp.vn>
2024-04-08 10:58:30 +0700

    Initial Commit

---
+++ TikTokCloneApp/TikTokCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneAppApp.swift
+//  TikTokCloneApp
+//
+//  Created by Nguyen Thanh Vinh on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
